7703094b009cb9ea762e59b5407f4089f63063fa
macbellingrath <macbellingrath@gmail.com>
2017-07-15 09:13:42 -0400

    adds exercise

---
+++ learning/30DaysOfCPP/three/exercise.cpp
@@ -0,0 +1,40 @@
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <ctime>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <string>
+#include <bitset>
+#include <cstdio>
+#include <limits>
+#include <vector>
+#include <climits>
+#include <cstring>
+#include <cstdlib>
+#include <fstream>
+#include <numeric>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+template<class T>
+bool check_range(T value, T min, T max) {
+    return (value >= min) && (value <= max);
+}
+
+int main() {
+  int N;
+   cin >> N;
+  // If n  is odd, print Weird
+  if (N % 2 != 0) {
+    cout << "Weird" << endl;
+  } else if ()
+  // If n is even and in the inclusive range of  to , print Not Weird
+  // If n is even and in the inclusive range of  to , print Weird
+  // If n is even and greater than , print Not Weird
+  return 0;
+}
