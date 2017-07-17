62e024a553a6a8c8d78d10f299e51c4b5305bfd8
Mac Bellingrath <mbellingrath@weddingwire.com>
2017-07-17 09:55:40 -0400

    complets exercise

--- learning/30DaysOfCPP/six/exercise.cpp
+++ learning/30DaysOfCPP/six/exercise.cpp
@@ -3,11 +3,30 @@
 #include <vector>
 #include <iostream>
 #include <algorithm>
-using namespace std;
 
+using namespace std;
 
 int main() {
    int numberOfCases;
-   std::cin >> numberOfCases;
+   cin >> numberOfCases;
+
+   for (int i = 1; i <= numberOfCases; i ++) {
+      string s;
+      cin >> s;
+
+      string evens = "";
+      string odds  = "";
+
+      for (int i = 0; i < s.length(); i++) {
+        if (i % 2 == 0) {
+          evens += s[i];
+        } else {
+          odds  += s[i];
+        }
+      }
+
+      cout << evens + " " + odds << endl;
+   }
+
     return 0;
 }
