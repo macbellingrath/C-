0e75dcf2b11185c9bcbd841bada5271e9657daf0
Mac Bellingrath <mbellingrath@weddingwire.com>
2017-07-17 09:30:35 -0400

    adds exercise

---
+++ learning/30DaysOfCPP/five/exercise.cpp
@@ -0,0 +1,18 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int x >> cin;
+    cin >> x;
+
+    for (int multiple = 1; multiple <= 10; multiple++){
+      int result = x * multiple;
+      cout << x << " x " << multiple << " = " << result << endl;
+    }
+
+    return 0;
+}
