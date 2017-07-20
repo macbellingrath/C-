3a0e3b8a4cb70720583e14864ba4e12cf6dab673
macbellingrath <macbellingrath@gmail.com>
2017-07-20 17:01:19 -0400

    adds exercise

---
+++ learning/30DaysOfCPP/nine/exercise.cpp
@@ -0,0 +1,21 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int factorial(int x) {
+    if (x <= 1) {
+        return 1;
+    } else {
+        return x * factorial(x - 1);
+    }
+}
+
+int main() {
+    int x;
+    cin >> x;
+    cout << factorial(x) << endl;
+    return 0;
+}
