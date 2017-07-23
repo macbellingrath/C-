9b470973bd62616a2bf244e973fa95c3436d69ef
macbellingrath <macbellingrath@gmail.com>
2017-07-23 13:07:19 -0400

    adds back person class

--- learning/30DaysOfCPP/twelve/exercise.cpp
+++ learning/30DaysOfCPP/twelve/exercise.cpp
@@ -3,34 +3,21 @@
 
 using namespace std;
 
+class Person{
+	protected:
+		string firstName;
+		string lastName;
+		int id;
+	public:
+		Person(string firstName, string lastName, int identification){
+			this->firstName = firstName;
+			this->lastName = lastName;
+			this->id = identification;
+		}
+		void printPerson(){
+			cout << "Name: "<< lastName << ", "<< firstName <<"\nID: "<< id << "\n";
+		}
 
-class Student: public Person {
-  private:
-    vector<int> testScores;
-  public:
-    Student(string firstName, string lastName, int id, vector<int> testScores) : Person(firstName, lastName, id) {
-      this->testScores = testScores;
-    }
-
-  string calculate() {
-    double a = 1.0 * accumulate(testScores.begin(), testScores.end(), 0.0)/ testScores.size();
-    int average = int(a);
-    string letter = "";
-    if (average >= 90 && average <= 100) {
-        letter = "O";
-    } else if (average >= 80 && average < 90) {
-        letter =  "E";
-    } else if (average >= 70 && average < 80){
-        letter =  "A";
-    } else if (average >= 55 && average < 70) {
-        letter =  "P";
-    } else if (average >= 40 && average < 55) {
-        letter =  "D";
-    } else {
-        letter = "T";
-    }
-      return letter;
-    }
 };
 
 #include <iostream>
@@ -68,6 +55,7 @@ class Student: public Person {
       return letter;
     }
 };
+
 
 int main() {
   string firstName;
