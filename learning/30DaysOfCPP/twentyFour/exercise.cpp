7571c4faa5b1e71905468aa5fc2a0e0e9bff7af6
Mac Bellingrath <mbellingrath@weddingwire.com>
2017-08-04 21:00:44 -0400

    updates exercise

--- learning/30DaysOfCPP/twentyFour/exercise.cpp
+++ learning/30DaysOfCPP/twentyFour/exercise.cpp
@@ -6,14 +6,16 @@
 //  Copyright © 2017 McmillanBellingrath. All rights reserved.
 //
 
-#include <iostream>
-#include <vector>
 #include <cstddef>
 #include <cmath>
-#include <set>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
 using namespace std;
 
-class Node {
+class Node
+{
 public:
     int data;
     Node *next;
@@ -23,37 +25,78 @@ public:
     }
 };
 
-
 class Solution{
 public:
-    Node* removeDuplicates(Node *head){
-        // check head for null
-        Node *h = head;
-        if (h == NULL) return h;
 
-        Node *current = head;
-        Node *next = current->next;
-        set<int> xs;
+    /**
+     removes duplicates from linked list
 
-        xs.insert(current->data);
+     @param head node nude
+     @return head node from updated list after duplicates have been removed
+     */
+    Node* removeDuplicates(Node *head)
+    {
+        Node* n = head;
 
-        while (current != NULL) {
-            // check if next node is in list
-            if (xs.find(next->data) != xs.end()) {
-                xs.insert(next->data);
-                current = next;
-                next = next->next;
-            } else {
-                current->next = next->next;
-                next = next->next;
+        while( n != NULL ) {
+
+            while( n->next != NULL && n->data == n->next->data ) {
+                Node* delete_me = n->next;
+                n->next = n->next->next;
+                delete delete_me;
             }
-        }
 
+            n = n->next;
+        }
         return head;
+    }
+
+    Node* insert(Node *head,int data)
+    {
+        Node* p=new Node(data);
+        if(head==NULL){
+            head=p;
+
+        }
+        else if(head->next==NULL){
+            head->next=p;
+
+        }
+        else{
+            Node *start=head;
+            while(start->next!=NULL){
+                start=start->next;
+            }
+            start->next=p;
+
+        }
+        return head;
+
+
+    }
+    void display(Node *head)
+    {
+        Node *start=head;
+        while(start)
+        {
+            cout<<start->data<<" ";
+            start=start->next;
+        }
     }
 };
 
+int main()
+{
+    Node* head=NULL;
+    Solution mylist;
+    int T,data;
+    cin>>T;
+    while(T-->0){
+        cin>>data;
+        head=mylist.insert(head,data);
+    }
+    head=mylist.removeDuplicates(head);
 
-int main() {
-    return 0;
+    mylist.display(head);
+
 }
